11ac2776aa8f4c1bec5835dd8fec2610c66cd998
Shahmir Faisal <shahmir049@gmail.com>
2022-11-05 12:03:40 +0500

    added mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,14 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import { CacheProvider } from "@emotion/react";
 import theme from "../src/theme";
-import createEmotionCache from "../src/createEmotionCache";
 import Head from "next/head";
 
-// Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
-
 export default function MyApp(props) {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const { Component, pageProps } = props;
 
   return (
-    <CacheProvider value={emotionCache}>
+    <>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
@@ -22,6 +17,6 @@ export default function MyApp(props) {
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </CacheProvider>
+    </>
   );
 }
